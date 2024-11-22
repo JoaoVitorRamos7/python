235d6391c4bcb8d950abe65264c4a4eb24120441
joao vitor <joaovitorramosdealmeida55@gmail.com>
2024-11-22 09:54:43 -0300

    adicionar  arquivo somar dois numeros

---
+++ somar-dois-numeros-inteiros.py
@@ -0,0 +1,10 @@
+# Funções
+# Variáveis 
+
+
+# fluxo do programa
+
+var1 = int(input('digite um número para somar: '))
+var2 = int(input('digite outro número para concluir somar: '))
+resultado = var1 + var2
+print(resultado)
